995655b7f008c7fc6a085830c4a3ecb074fa6de6
= <=>
2017-05-23 22:53:07 +0100

    Twig Layouts

---
+++ src/controllers/ShortenController.php
@@ -0,0 +1,15 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Daniel
+     * Date: 23/05/2017
+     * Time: 22:41
+     */
+
+    namespace net\devtales\controllers;
+
+
+    class ShortenController
+    {
+
+    }
